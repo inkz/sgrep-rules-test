4193f514f1041e2104753890f58e04f72c041969
inkz <inkz@xakep.ru>
2020-03-22 16:26:14 +1000

    remove old files

--- jwt-hardcode.js
+++
@@ -1,6 +0,0 @@
-const jwt = require('jsonwebtoken')
-
-jwt.sign({"foo": 2}, "hardcode1", () => {})
-
-var secret = "hardcode2"
-jsonwebtoken.sign({"bar": 2}, secret)
--- jwt-hardcode.py
+++
@@ -1,7 +0,0 @@
-import jsonwebtoken as jwt
-
-SECRET = "secret123"
-
-jwt.sign({}, SECRET)
-jwt.sign({}, "one-more-secret")
-return 0
